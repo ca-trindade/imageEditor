7326897e5708a3a45ba3bd5ffa21efc24430c481
ca-trindade <piratum.core@gmail.com>
2024-02-07 16:08:49 +0000

    working with sharp library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import ServerUploadPage from "./server/page";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <ImageUploader /> */}
       <ServerUploadPage />
+      
     </main>
   );
 }
--- src/app/server/page.tsx
+++ src/app/server/page.tsx
@@ -1,34 +1,38 @@
-import { writeFile } from "fs/promises";
-import { join } from "path"
+// import { writeFile } from "fs/promises";
+import { join } from "path";
+import os from "os";
+import sharp from "sharp";
 
 export default function ServerUploadPage() {
+  async function upload(data: FormData) {
+    "use server";
+    try {
+    const file: File | null = data.get("file") as unknown as File;
 
-    async function upload(data: FormData) {
-        'use server'
-        const file: File | null = data.get('file') as unknown as File
+    if (!file) {
+      throw new Error("No file uploaded");
+    }
 
-        if (!file) {
-            throw new Error('No file uploaded')
-        }
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
 
-        const bytes = await file.arrayBuffer()
-        const buffer = Buffer.from(bytes)
+    const desktopPath = join(os.homedir(), "Desktop");
+    const outputPath = join(desktopPath, "edited.jpeg");
+    const sharpImage = await sharp(buffer).blur(15).toFile(outputPath);
 
-        const path = join('--path--', file.name)
-        await writeFile(path, buffer)
-        console.log(`open ${path} to see uploaded file`)
-
-        return {success: true}
-
+      return { success: true }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-    return (
-        <main>
-            <h1>Server Component</h1>
-            <form action={upload}>
-<input type="file" name="file" />
-<input type="submit" value="Upload" />
-            </form>
-        </main>
-    )
+  return (
+    <main>
+      <h1>Server Component</h1>
+      <form action={upload}>
+        <input type="file" name="file" />
+        <input type="button" value="Upload" />
+      </form>
+    </main>
+  );
 }
